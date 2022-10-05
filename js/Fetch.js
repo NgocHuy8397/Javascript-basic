2781108c02b3aee6e88234f0eeb3ca6e59402e64
NgocHuy8397 <ngochuy8397@gmail.com>
2022-10-06 00:17:55 +0700

    update

--- js/Fetch.js
+++ js/Fetch.js
@@ -9,26 +9,26 @@
 // -> JSON.pare -> Javascript types
 // -> Render ra giao diện với HTML
 
-var postApi = "https://jsonplaceholder.typicode.com/posts";
+// var postApi = "https://jsonplaceholder.typicode.com/posts";
 
-fetch(postApi)
-    .then(function(response) {
-        return response.json();
-        // JSON.parse: JSON -> Javascript type
-    })
-    .then(function(posts){
-        // console.log(posts);
-        var htmls = posts.map(function(post){
-            return `
-                <li>
-                <h2>${post.title}</h2>
-                <p>${post.body}</p>
-                </li>            
-            `;
-        });
-      var html =  htmls.join('');
-      document.getElementById('post-block').innerHTML = html;
-    })
-    .catch(function(err) {
-        alert("có lỗi !");
-    });
+// fetch(postApi)
+//     .then(function(response) {
+//         return response.json();
+//         // JSON.parse: JSON -> Javascript type
+//     })
+//     .then(function(posts){
+//         // console.log(posts);
+//         var htmls = posts.map(function(post){
+//             return `
+//                 <li>
+//                 <h2>${post.title}</h2>
+//                 <p>${post.body}</p>
+//                 </li>            
+//             `;
+//         });
+//       var html =  htmls.join('');
+//       document.getElementById('post-block').innerHTML = html;
+//     })
+//     .catch(function(err) {
+//         alert("có lỗi !");
+//     });
